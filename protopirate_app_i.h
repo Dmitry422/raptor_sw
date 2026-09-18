a6927f6f12433a793a8d78b19a090538576b85fe
Leeroy <135471162+LeeroysHub@users.noreply.github.com>
2026-09-18 12:14:17 +1000

    Restore PSA BF pointers

--- protopirate_app_i.h
+++ protopirate_app_i.h
@@ -109,6 +109,8 @@ struct ProtoPirateApp {
     uint8_t emulate_nav_pending;
 #endif
     const ProtoPirateConfigPlugin* config_plugin;
+    CompositeApiResolver* psa_bf_plugin_resolver;
+    PluginManager* psa_bf_plugin_manager;
     const ProtoPiratePsaBfPlugin* psa_bf_plugin;
     const ProtoPirateToolScenePlugin* tool_scene_plugin;
     ProtoPirateToolScenePluginKind tool_scene_plugin_kind;
